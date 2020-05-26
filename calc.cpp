e3be17e07b65232574d18054e8d286809f5307cd
Aissa <aissa.hadjoudja@gmail.com>
2020-05-26 16:52:37 +0200

    Ajout de Print current date

--- calc.cpp
+++ calc.cpp
@@ -1,9 +1,22 @@
-
 #include "calc.hpp"
+#include <chrono>
+#include <iostream>
+#include <date/date.h>
 
 int mavar_globale;
 
 int add(int i, int j)
 {
-    return i + i;
+return i + i;
+}
+void print_current_date()
+
+{
+auto nombre_de_secondes_depuis_1970 =std::chrono::system_clock::now();
+auto nombre_de_secondes_depuis_1970 =date::floor<date::days>(nombre_de_secondes_depuis_1970);
+auto anne_mois_jour = date::year_month_day(nombre_de_secondes_depuis_1970);
+std::cout << anne_mois_jour;
+
+
+
 }
